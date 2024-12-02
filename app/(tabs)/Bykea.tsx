c04983244df781298f3283018899280ae3b7cb56
Muttahar Sheikh <153789569+muttahar123@users.noreply.github.com>
2024-12-02 12:47:55 +0500

    commit

---
+++ app/(tabs)/Bykea.tsx
@@ -0,0 +1,14 @@
+import { ThemedText } from "@/components/ThemedText";
+import { View } from "react-native";
+
+
+
+export default function BykeaScreen(){
+    return(
+        <View style={{flex:1, justifyContent: "center"}}>
+            <ThemedText>
+                Bykea Screen
+            </ThemedText>
+        </View>
+    )
+}
